72d76a51debc94a5c48e70353ec1557b8c4254cb
dhfromkorea <dhllee424@gmail.com>
2015-07-10 19:34:28 -0700

    new modular folder structure

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -5,14 +5,42 @@ var express = require('express'),
 
 module.exports = function (app) {
   app.use('/', router);
+
+  app.get('/reviews', function(req, res){
+    // read a list of reviews
+    var univId = req.params.univId;
+
+  });
+
+  app.post('/reviews', function(){
+
+  });
+
+  app.get('/reviews/:id', function(){
+
+  });
+
+  app.get('/universities', function(res, req){
+
+  });
+
+  app.get('/universities/:id', function(res, req){
+    // todo: later
+  });
+
+  app.post('/universities', function(res, req){
+    // todo: later
+  });
+
 };
 
-router.get('/', function (req, res, next) {
-  Article.find(function (err, articles) {
-    if (err) return next(err);
-    res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
-    });
-  });
-});
+
+// router.get('/', function (req, res, next) {
+//   Article.find(function (err, articles) {
+//     if (err) return next(err);
+//     res.render('index', {
+//       title: 'Generator-Express MVC',
+//       articles: articles
+//     });
+//   });
+// });
