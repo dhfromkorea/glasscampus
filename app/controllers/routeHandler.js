777401a0fe8f8d3f67323bbb271c30c7958ac930
Joost Ouwerling <joost.ouwerling@mobiyou.nl>
2015-07-11 09:41:26 -0700

    added example route implementation

--- app/controllers/routeHandler.js
+++ app/controllers/routeHandler.js
@@ -1,44 +1,48 @@
+var mongoose = require('mongoose');
+var University = require('../models/university.js');
+var Review = require('../models/review.js');
+
 module.exports.getIndex = function(req, res){
-  res.render('index');
+	res.render('index');
 };
 
 module.exports.getReviews = function(req, res){
-    // univId = user-selected university
-    var univId = req.params.id || '';
-    // find reviews
-
-    // onsuccess
-    res.json(reviews);
-    // onerror
-
+	// univId = user-selected university
+	var univId = req.params.id || '';
+	// find reviews
+	if(univId) {
+		Review.findById(univId, successCallback, errorCallback);
+	} else {
+		Review.findAll(successCallback, errorCallback);
+	}
 };
+
 module.exports.getReview = function(req, res){
-    var reviewId = req.params.id || '';
-    // find the review
-    // onsuccess
-    res.json(review);
-    // onerror
+	var reviewId = req.params.id || '';
+	// find the review
+	// onsuccess
+	res.json(review);
+	// onerror
 };
 module.exports.createReview = function(req, res){
-    var review = req.body.data;
-
-    // onsuccess
-    res.send(newReview);
+	var review = req.body.data;
+	// onsuccess
+	res.send(newReview);
 };
 module.exports.getUnivs = function(req, res){
-    // get all univs
+	// get all univs
 
-    // onsucesss
-    res.json(univs);
+	// onsucesss
+	res.json(univs);
 };
 module.exports.getUniv = function(req, res){
-    var univId = req.params.id || '';
-    // onsuccess
-    res.json(univ);
+	var univId = req.params.id || '';
+	// onsuccess
+	res.json(univ);
 };
 module.exports.createUniv = function(req, res){
-    var univ = req.body.data;
+	var univ = req.body.data;
 
-    // onsuccess
-    res.send(newUniv);
+	// onsuccess
+	res.send(newUniv);
 };
